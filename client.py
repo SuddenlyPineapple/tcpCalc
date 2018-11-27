2e1812e4108edf63dbf1b90aeac3972ad5b4c93b
KalinZeglarz <44393752+KalinZeglarz@users.noreply.github.com>
2018-11-28 00:30:15 +0100

    Update client.py

--- client.py
+++ client.py
@@ -24,18 +24,18 @@ class Client:
         # Zmienne Clienta
 
         error = False
-        number1 = 0
-        while number1 is not int or number1 is not float:
+        number1 = 'abc'
+        while number1 is str:
             if error:
                 number1 = input('It is not a number! Please choose the number: ')
             else:
                 number1 = input("Choose the first number: ")
-                if number1 is not int or number1 is not float:
+                if number1 is str:
                     error = True
 
         error = False
         oper = 0
-        while oper is not str or oper is not 'addition'or oper is not 'subtraction'or oper is not 'multiplication'or oper is not 'division'or oper is not 'exponentiation'or oper is not 'negation'or oper is not 'root'or oper is not 'combination'or oper is not 'factorial':
+        while oper is not 'addition'or oper is not 'subtraction'or oper is not 'multiplication'or oper is not 'division'or oper is not 'exponentiation'or oper is not 'negation'or oper is not 'root'or oper is not 'combination'or oper is not 'factorial':
             if error:
                 oper = input('It is not an operation! Please choose one: ')
             else:
@@ -50,17 +50,17 @@ class Client:
                 print('combination -kombinacja')
                 print('factorial -silnia')
                 oper = input("Choose the operation: ")
-                if oper is not str or oper is not 'addition'or oper is not 'subtraction'or oper is not 'multiplication'or oper is not 'division'or oper is not 'exponentiation'or oper is not 'negation'or oper is not 'root'or oper is not 'combination'or oper is not 'factorial':
+                if oper is not 'addition'or oper is not 'subtraction'or oper is not 'multiplication'or oper is not 'division'or oper is not 'exponentiation'or oper is not 'negation'or oper is not 'root'or oper is not 'combination'or oper is not 'factorial':
                     error = True
 
         error = False
-        number2 = 0
-        while number2 is not int or number2 is not float:
+        number2 = 'abc'
+        while number2 is str:
             if error:
                 number2 = input('It is not a number! Please choose the number: ')
             else:
                 number2 = input("Choose the first number: ")
-                if number2 is not int or number2 is not float:
+                if number2 is str:
                     error = True
         try:
             sock.sendall(Segment(OPERATION.addition, "wynik", 1, time.time(), 123, -123, "").pack())
