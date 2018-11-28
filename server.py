7812341c991f885bf07b940bef86277e7ee6afd5
Wojciech Kasperski <wojciech.kasperski97@gmail.com>
2018-11-28 01:27:47 +0100

    Server ops added

--- server.py
+++ server.py
@@ -1,5 +1,8 @@
 import _thread
+import random
 import socket
+import time
+import functions
 
 from operations import OPERATION
 from segment import Segment
@@ -20,17 +23,47 @@ class Server:
             while True:
                 data = connection.recv(4096)
 
-
                 if len(data) != 0:
                     message = Segment.unpack(data)
                     message.print()
 
                     print('Received: ' + str(data))
+                    token = message.id
+                    numberA = message.numberA
+                    numberB = message.numberB
 
-                    if message.operation == OPERATION.addition:
-                        print("Test")
+                    if message.operation == OPERATION.id:
+                        token = random.randint(100, 1000)
+                        connection.sendall(Segment(OPERATION.id, True, token, time.time(), "", "", ""))
+                    elif message.operation == OPERATION.addition:
+                        result = functions.addition(numberA, numberB)
+                        connection.sendall(Segment(OPERATION.id, True, token, time.time(), 0, 0, result))
+                    elif message.operation == OPERATION.subtraction:
+                        result = functions.subtraction(numberA, numberB)
+                        connection.sendall(Segment(OPERATION.id, True, token, time.time(), 0, 0, result))
+                    elif message.operation == OPERATION.multiplication:
+                        result = functions.multiplication(numberA, numberB)
+                        connection.sendall(Segment(OPERATION.id, True, token, time.time(), 0, 0, result))
+                    elif message.operation == OPERATION.division:
+                        result = functions.division(numberA, numberB)
+                        connection.sendall(Segment(OPERATION.id, True, token, time.time(), 0, 0, result))
+                    elif message.operation == OPERATION.exponentiation:
+                        result = functions.exponentiation(numberA, numberB)
+                        connection.sendall(Segment(OPERATION.id, True, token, time.time(), 0, 0, result))
+                    elif message.operation == OPERATION.negation:
+                        result = functions.negation(numberA)
+                        connection.sendall(Segment(OPERATION.id, True, token, time.time(), 0, 0, result))
+                    elif message.operation == OPERATION.root:
+                        result = functions.root(numberA, numberB)
+                        connection.sendall(Segment(OPERATION.id, True, token, time.time(), 0, 0, result))
+                    elif message.operation == OPERATION.combination:
+                        result = functions.combination(numberA, numberB)
+                        connection.sendall(Segment(OPERATION.id, True, token, time.time(), 0, 0, result))
+                    elif message.operation == OPERATION.factorial:
+                        result = functions.factorial(numberA)
+                        connection.sendall(Segment(OPERATION.id, True, token, time.time(), 0, 0, result))
                     else:
-                        print('Error: Bad flags settings!')
+                        print('Error occured: Wizard must be stopped!')
 
                     # break
 
