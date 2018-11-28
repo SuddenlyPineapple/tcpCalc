23b44ce204d430ab4e6d8b2d8647e3665f7e290d
Wojciech Kasperski <wojciech.kasperski97@gmail.com>
2018-11-28 02:50:11 +0100

    tcpCalc is fully functional

--- client.py
+++ client.py
@@ -21,50 +21,77 @@ class Client:
         print('Connecting to %s port %s' % server_address)
         sock.connect(server_address)
 
-        # Zmienne Clienta
-        ops = ("addition", "subtraction", "multiplication", "division", "exponentiation", "negation", "root", "combination", "factorial")
-        ops2 = ("addition", "subtraction", "multiplication", "division", "exponentiation", "root", "combination")
-        while True:
-            try:
-                print('Operations: ')
-                print('addition -dodawanie')
-                print('subtraction -odejmowanie')
-                print('multiplication -mnozenie')
-                print('division -dzielenie')
-                print('exponentiation -potegowanie')
-                print('negation -negowanie')
-                print('root -pierwiastkowanie')
-                print('combination -kombinacja')
-                print('factorial -silnia')
-                op = str(input("Choose the operation: "))
-                assert(op in ops)
-                break
-            except:
-                print("Operation not found!")
-
-        while True:
-            try:
-                number1 = int(input("Enter first number:"))
-                assert (number1 < 2147483647 or number1 > -2147483648), 'Number must be in int range'
-                break
-            except:
-                print("This is a string!")
-
-        if op in ops2:
-            while True:
-                try:
-                    number2 = int(input("Enter second number:"))
-                    assert (number2 < 2147483647 or number2 > -2147483648), 'Number must be in int range'
-                    break
-                except:
-                    print("This is a string")
         try:
-            sock.sendall(Segment(OPERATION(op), True, 0, time.time(), number1, number2, "").pack())
-            while True:
-                data = sock.recv(4096)
-                if len(data) != 0:
-                    message = Segment.unpack(data)
-                    message.print()
+            token = 0
+            sock.sendall(Segment(OPERATION.id, True, token, time.time(), 0, 0, 0).pack())
+            data = sock.recv(4096)
+            message = Segment.unpack(data)
+            token = message.id
+            print("Connection Identificator is: #" + str(token))
+
+            loop = 1
+            while loop == 1:
+                # Zmienne tablicowe do rozrozniania typow operacji na jedno i dwu argumentowe
+                ops = ("addition", "subtraction", "multiplication", "division", "exponentiation", "negation", "root", "combination", "factorial")
+                ops2 = ("addition", "subtraction", "multiplication", "division", "exponentiation", "root", "combination")
+                while True:
+                    try:
+                        print('Operations: ')
+                        print('addition -dodawanie')
+                        print('subtraction -odejmowanie')
+                        print('multiplication -mnozenie')
+                        print('division -dzielenie')
+                        print('exponentiation -potegowanie')
+                        print('negation -negowanie')
+                        print('root -pierwiastkowanie')
+                        print('combination -kombinacja')
+                        print('factorial -silnia')
+                        op = str(input("Choose the operation: "))
+                        assert(op in ops)
+                        break
+                    except:
+                        print("Operation not found!")
+
+                while True:
+                    try:
+                        number1 = int(input("Enter first number:"))
+                        number2 = 0
+                        assert (2147483647 > number1 > -2147483648), 'Number must be in variable range (int/float)'
+                        break
+                    except:
+                        print("This is a string or number not fit in variable size!")
+
+                if op in ops2:
+                    while True:
+                        try:
+                            number2 = int(input("Enter second number:"))
+                            assert (2147483647 > number2 > -2147483648), 'Number must be in variable range (int/float)'
+                            break
+                        except:
+                            print("This is a string or number not fit in variable size!")
+
+
+                sock.sendall(Segment(OPERATION(op), True, token, time.time(), number1, number2, "").pack())
+                while True:
+                    data = sock.recv(4096)
+                    if len(data) != 0:
+                        message = Segment.unpack(data)
+                        #message.print()
+                        if message.status == "False":
+                            print("Result is larger than variable size, please consider smaller numbers next time!")
+                        else:
+                            print("Result of " + str(message.operation.value) + " is: " + str(message.result))
+                        break
+
+                while True:
+                    try:
+                        answer = input("Wanna do another calculations? [y - yes, n - no]")
+                        assert (answer == "y" or answer == "n"), 'Wrong answer! Try again!'
+                        if answer == "n":
+                            loop = 0
+                        break
+                    except:
+                        print("Wrong answer! Try again!")
 
         finally:
             print('Closing socket')
